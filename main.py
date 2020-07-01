49ce8a4a730ca11ce913cff920738d6d08d74908
Trojanekkk <W.Trojan27@gmail.com>
2020-07-01 13:20:51 +0200

    Add: reducers; Edit: Calculate prediction for every reducer; Calculate score for every reducer

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RepeatedStratifiedKFold
 from linear_classifier import linear_classifier
 from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
 # Jakość predykcji
 def accuracy (y, y_pred):
@@ -18,6 +20,13 @@ def accuracy (y, y_pred):
     
     return points / len(y_pred)
 
+# Stworzenie słownika klas redukcji dla zadanej liczby wymiarów
+dimension = 3
+reducers = {
+    "pca" : PCA(n_components=dimension),
+    "anova" : SelectKBest(score_func=f_classif, k=dimension)
+}
+
 # Importowanie zbioru danych, wydzielenie etykiet i wzorców
 dataset = np.genfromtxt("datasets/australian.csv", delimiter=",")
 X = dataset[:, :-1]
@@ -29,33 +38,39 @@ scaler.fit(X)
 X = scaler.transform(X)
 
 # Stworzenie obiektu do obsługi foldów
-rskf = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=1410)
+folds = 5
+repeats = 10
+rskf = RepeatedStratifiedKFold(n_splits=folds, n_repeats=repeats, random_state=1410)
 
 # Inicjalizowanie nowego obiektu
 lc = linear_classifier()
 
-scores = []
+scores = [[],[]]
 # Podział zbiorów wg stratyfikowanej k-foldowej walidacji z powtórzeniami
 for train_i, test_i in rskf.split(X, y):
-    X_train = X[train_i]
-    y_train = y[train_i]
-    X_test = X[test_i]
-    y_test = y[test_i]
+    
+    #Wybór naprzemiennie PCA i ANOVA do redukcji kolejnych foldów
+    for reducer_i, reducer in enumerate(reducers):
+        reducer_obj = reducers[reducer]
+        X_tmp = reducer_obj.fit_transform(X, y)
+        X_train = X_tmp[train_i]
+        y_train = y[train_i]
+        X_test = X_tmp[test_i]
+        y_test = y[test_i]
 
-    # Uczenie
-    lc.fit(X_train, y_train)
+        # Uczenie
+        lc.fit(X_train, y_train)
 
-    # Predykcja
-    y_pred = lc.predict(X_test)
+        # Predykcja
+        y_pred = lc.predict(X_test)
 
-    # Obliczenie jakości dla uzyskanej predykcji
-    score = accuracy(y_test, y_pred)
-    scores.append(score)
+        # Obliczenie jakości dla uzyskanej predykcji
+        score = accuracy(y_test, y_pred)
+        scores[reducer_i].append(score)
 
-# Wyznaczenie średniej jakości
-mean_score = sum(scores) / len(scores)
-print(mean_score)
+# Wyznaczenie średniej jakości dla metod redukcji
+for reducer_score_i, reducer_score in enumerate(scores):
+    mean_score = sum(scores[reducer_score_i]) / len(scores[reducer_score_i])
+    print(mean_score)
 
-# Przeprowadzenie PCA, ANOVA
-# Obliczenie jakości
 # Porównanie jakości dla PCA, ANOVA (testy parowe, testy globalne na 7 rangach)
